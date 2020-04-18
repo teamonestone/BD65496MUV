71637d2b15cc870b52fe402358e4cd3bf27f1502
Jonas Merkle [JJM] <jonas@jjm.one>
2020-04-18 20:55:21 +0200

    lib implemented

--- src/BD65496MUV.cpp
+++ src/BD65496MUV.cpp
@@ -6,8 +6,8 @@
  * @author
  * This library is maintained by <a href="https://team-onestone.net">Team Onestone</a>.
  * E-Mail: <a href="mailto:info@team-onestone.net">info@team-onestone.net</a>
- * @version 1.0.2
- * @date 30 October 2019
+ * @version 1.0.0
+ * @date 18 April 2020
  * @copyright This project is released under the GNU General Public License v3.0
  */
 
@@ -42,53 +42,316 @@
 /////////////
 #include "BD65496MUV.h"
 
+
 /////////////
 // defines //
 /////////////
+#define _BD65496MUV_LIB_VERSION 100     ///< The version number of the library.
+
 
 //////////////////
 // constructors //
 //////////////////
 
 /**
- * @brief Constructor of the BD65496MUV class without the pwm mode pin.
- */
-BD65496MUV::BD65496MUV(Mode mode, uint8_t pinInA, uint8_t pinInB) {
-    
-    // setting private variables
-    _driverMode = mode;
-    _pinInA = pinInA;
-    _pinInB = pinInB;
-    _pinPwmMode = 0;
-
-    _initDriverPins();
-}
-
-/**
  * @brief Constructor of the BD65496MUV class with the pwm mode pin.
+ * 
+ * @param driverMode The driver mode of the driver (Speed_n_Dir or H_Bridge).
+ * @param pinInA The pin number to which the INA pin is connected.
+ * @param pinInB The pin number to which the INB pin is connected.
+ * @param pinPwmMode The pin number to wicht the pmw mode pin is connected.
  */
-BD65496MUV::BD65496MUV(Mode mode, uint8_t pinInA, uint8_t pinInB, uint8_t pinPwmMode) {
+BD65496MUV::BD65496MUV(uint8_t pinInA, uint8_t pinInB, uint8_t pinPwmMode, DriverMode driverMode) {
 
     // setting private variables
-    _driverMode = mode;
+    _driverMode = driverMode;
+    _powerMode = PowerMode::Running;
+    _switchingSpeed = SwitchingSpeed::On150_Off50;
+    _shortBreakMode = ShortBreakMode::Off;
+    _pinPwmModeDefined = true;
+    _pinPsDefined = false;
+    _pinTrDefined = false;
     _pinInA = pinInA;
     _pinInB = pinInB;
     _pinPwmMode = pinPwmMode;
+    _pinPs = 0;
+    _pinTr1 = 0;
+    _pinTr2 = 0;
+    _speed = 0;
 
+    // init pins
     _initDriverPins();
 }
 
 /**
- * @brief Main destructor  of the BD65496MUV class.
+ * @brief Constructor of the BD65496MUV class with all pins (Pro-Mode).
+ * 
+ * @param driverMode The driver mode of the driver (Speed_n_Dir or H_Bridge).
+ * @param pinInA The pin number to which the INA pin is connected.
+ * @param pinInB The pin number to which the INB pin is connected.
+ * @param pinPwmModeDefined Flag that defines if the pwm mode pin is connected.
+ * @param pinPwmMode The pin number to which the pmw mode pin is connected.
+ * @param pinPsDefined Flag that defines if the PS pin is connected.
+ * @param pinPs The pin number to which the PS pin is connected.
+ * @param powerMode The power mode of the driver (Running or Standby).
+ * @param pinTrDefined Flag that defines if the TR pins are connected.
+ * @param pinTr1 The pin number to which the TR1 pin is connected.
+ * @param pinTr2 The pin number to which the TR2 pin is connected.
+ * @param On150_Off50 The switching speed of the driver (On150_Off50, On250_Off70, On350_Off90, On500_Off110).
+ * @param shortBreakMode The short break mode of the driver (On or Off).
  */
-BD65496MUV::~BD65496MUV() {
-    ;
+BD65496MUV::BD65496MUV(uint8_t pinInA, uint8_t pinInB, DriverMode driverMode, 
+                    bool pinPwmModeDefined, uint8_t pinPwmMode,
+                    bool pinPsDefined, uint8_t pinPs, PowerMode powerMode,
+                    bool pinTrDefined, uint8_t pinTr1, uint8_t pinTr2,
+                    SwitchingSpeed switchingSpeed,
+                    ShortBreakMode shortBreakMode) {
+
+    // setting private variables
+    _driverMode = driverMode;
+    _powerMode = powerMode;
+    _switchingSpeed = switchingSpeed;
+    _shortBreakMode = shortBreakMode;
+    _pinPwmModeDefined = pinPwmModeDefined;
+    _pinPsDefined = pinPsDefined;
+    _pinTrDefined = pinTrDefined;
+    _pinInA = pinInA;
+    _pinInB = pinInB;
+    _pinPwmMode = pinPwmMode;
+    _pinPs = pinPs;
+    _pinTr1 = pinTr1;
+    _pinTr2 = pinTr2;
+    _speed = 0;
+
+    // init pins
+    _initDriverPins();
 }
 
-///////////////
-// functions //
-///////////////
 
+//////////////////////
+// public functions //
+//////////////////////
+
+/**
+ * @brief Set the speed of the motor.
+ * 
+ * @param speed The speed of the motor (from -255 to 255).
+ */
+void BD65496MUV::setSpeed(int16_t speed) {
+
+    // get trun direction
+    TurnDir trunDir;
+    if (speed < 0) {
+        
+        speed = speed * (-1);
+        trunDir = TurnDir::CCW;
+    }
+    else {
+
+        trunDir = TurnDir::CW;
+    }
+
+    // check speed
+    if (speed > 255) return;
+
+    switch (_driverMode)
+    {
+        case Speed_n_Dir:
+        
+            // check for short break mode on and speed 0
+            if (_shortBreakMode == ShortBreakMode::On && speed == 0) {
+                
+                digitalWrite(_pinInA, 0);
+                digitalWrite(_pinInB, 0);
+            }
+            // normale mode
+            else {
+                
+                analogWrite(_pinInA, speed);
+                digitalWrite(_pinInB, trunDir == TurnDir::CW ? 0 : 1);
+            }
+
+        break;
+    
+        case H_Bridge:
+
+            // check for short break mode on and speed 0
+            if (_shortBreakMode == ShortBreakMode::On && speed == 0) {
+                
+                digitalWrite(_pinInA, 1);
+                digitalWrite(_pinInB, 1);
+            }
+            // check for short break mode off and speed 0
+            else if (_shortBreakMode == ShortBreakMode::Off && speed == 0) {
+                
+                digitalWrite(_pinInA, 0);
+                digitalWrite(_pinInB, 0);
+            }
+            else {
+                
+                // check trun dir
+                if (trunDir == TurnDir::CW) {
+                    
+                    analogWrite(_pinInA, speed);
+                    digitalWrite(_pinInB, 0);
+                }
+                else {
+
+                    digitalWrite(_pinInA, 0);
+                    analogWrite(_pinInB, speed);
+                }
+            }
+
+        break;
+    }
+}
+
+/**
+ * @brief Get the speed of the moter.
+ * 
+ * @return The current speed of the motor.
+ */
+int16_t BD65496MUV::getSpeed() {
+    return _speed;
+}
+
+/**
+ * @brief Set the driver mode of the driver.
+ * 
+ * @param driverMode The driver mode (Speed_n_Dir or H_Bridge).
+ */
+void BD65496MUV::setDriverMode(BD65496MUV::DriverMode driverMode) {
+    
+    // set private vars
+    _driverMode = driverMode;
+
+    // check if the needed pins are defined
+    if (_pinPwmModeDefined) {
+
+        // set output to set the driver mode
+        _driverMode == DriverMode::H_Bridge ? digitalWrite(_pinPwmMode, 0) : digitalWrite(_pinPwmMode, 1);
+    }
+}
+
+/**
+ * @brief Get the dirver mdoe of the driver.
+ * 
+ * @return The current driver mode of the driver.
+ */
+BD65496MUV::DriverMode BD65496MUV::getDriverMode() {
+    return _driverMode;
+}
+
+/**
+ * @brief Set the power mode of the driver.
+ * 
+ * @param powerMode The power mode (Running or Standby).
+ */
+void BD65496MUV::setPowerMode(BD65496MUV::PowerMode powerMode) {
+
+    // set private vars
+    _powerMode = powerMode;
+
+    // check if the needed pins are defined
+    if (_pinPsDefined) {
+
+        // set output to set the power mode
+        _powerMode == PowerMode::Running ? digitalWrite(_pinPs, 1) : digitalWrite(_pinPs, 0);
+    }
+}
+
+/**
+ * @brief Get the power mode of the driver.
+ * 
+ * @return The current power mode of the driver.
+ */
+BD65496MUV::PowerMode BD65496MUV::getPowerMode() {
+    return _powerMode;
+}
+
+/**
+ * @brief Set the switching speed of the driver.
+ * 
+ * @param switchingSpeed The switching speed (On150_Off50, On250_Off70, On350_Off90, On500_Off110).
+ */
+void BD65496MUV::setSwitchingSpeed(BD65496MUV::SwitchingSpeed switchingSpeed) {
+
+    // set private vars
+    _switchingSpeed = switchingSpeed;
+
+    // check if the needed pins are defined
+    if (_pinTrDefined) {
+
+        // set output to set the switching speed
+        switch (_switchingSpeed) {
+
+            case SwitchingSpeed::On150_Off50 :
+                digitalWrite(_pinTr1, 0);
+                digitalWrite(_pinTr2, 0);
+            break;
+
+            case SwitchingSpeed::On250_Off70 :
+                digitalWrite(_pinTr1, 1);
+                digitalWrite(_pinTr2, 0);
+            break;
+
+            case SwitchingSpeed::On350_Off90 :
+                digitalWrite(_pinTr1, 0);
+                digitalWrite(_pinTr2, 1);
+            break;
+
+            case SwitchingSpeed::On500_Off110 :
+                digitalWrite(_pinTr1, 1);
+                digitalWrite(_pinTr2, 1);
+            break;
+        }
+    }
+}
+
+/**
+ * @brief Get the switching speed of the driver.
+ * 
+ * @return The current switching speed of the driver.
+ */
+BD65496MUV::SwitchingSpeed BD65496MUV::getSwitchingSpeed() {
+    return _switchingSpeed;
+}
+
+/**
+ * @brief Set the short break mode of the driver.
+ * 
+ * @param shortBreakMode The short break mode (On or Off).
+ */
+void BD65496MUV::setShortBreakMode(BD65496MUV::ShortBreakMode shortBreakMode) {
+    _shortBreakMode = shortBreakMode;
+}
+
+/**
+ * @brief Get the short break mode of the dirver.
+ * 
+ * @return The current short break mode of the driver.
+ */
+BD65496MUV::ShortBreakMode BD65496MUV::getShortBreakMode() {
+    return _shortBreakMode;
+}
+
+/**
+ * @brief Get the version of the library.
+ * 
+ * @return The current version of the library.
+ */
+uint16_t BD65496MUV::getVersion() {
+    return _BD65496MUV_LIB_VERSION;
+}
+
+
+///////////////////////
+// private functions //
+///////////////////////
+
+/**
+ * @brief Init the gpio pins to which the driver is connected to.
+ */
 void BD65496MUV::_initDriverPins() {
 
     // basic pins
@@ -98,20 +361,33 @@ void BD65496MUV::_initDriverPins() {
     digitalWrite(_pinInB, 0);
 
     // mode pin
-    if (_pinPwmMode != 0) {
+    if (_pinPwmModeDefined) {
 
+        // set pin mode
         pinMode(_pinPwmMode, OUTPUT);
 
         // set the driver mode
-        _driverMode == Mode:H_Bridge ? digitalWrite(_pinPwmMode, 0) : digitalWrite(_pinPwmMode, 1);
+        setDriverMode(_driverMode);
     }
-}
 
-/**
- * @brief Get the version of the library.
- * 
- * @return the current version of the library.
- */
-uint16_t BD65496MUV::get_version() {
-    return _BD65496MUV_LIB_VERSION;
+    // ps pin
+    if (_pinPsDefined) {
+        
+        // set pin mode
+        pinMode(_pinPs, OUTPUT);
+
+        // set the power mode
+        setPowerMode(_powerMode);
+    }
+
+    // tr pins
+    if (_pinTrDefined) {
+
+        // set pin mode
+        pinMode(_pinTr1, OUTPUT);
+        pinMode(_pinTr2, OUTPUT);
+
+        // set the switching speed
+        setSwitchingSpeed(_switchingSpeed);
+    }
 }
--- src/BD65496MUV.h
+++ src/BD65496MUV.h
@@ -7,19 +7,28 @@
  * This library is maintained by <a href="https://team-onestone.net">Team Onestone</a>.
  * E-Mail: <a href="mailto:info@team-onestone.net">info@team-onestone.net</a>
  * @version 1.0.0
- * @date 17 December 2019
+ * @date 18 April 2020
  * @copyright This project is released under the GNU General Public License v3.0
  */
 
 #ifndef BD65496MUV_h
 #define BD65496MUV_h
 
-// includes
+//////////////
+// includes //
+//////////////
 #include <inttypes.h>
 #include <Arduino.h>
 
-// defines
-#define _BD65496MUV_LIB_VERSION 100	///< The version number of the library.
+
+/////////////
+// defines //
+/////////////
+
+
+/////////////
+// classes //
+/////////////
 
 /**
  * @class BD65496MUV
@@ -29,30 +38,68 @@ class BD65496MUV {
 // Begin PUBLIC ------------------------------------------------------------------
     public:
 
-        enum Mode {H_Bridge, Speed_n_Dir};
+        // enums
+        enum DriverMode {Speed_n_Dir, H_Bridge};
+        enum PowerMode  {Running, Standby};
+        enum SwitchingSpeed {On150_Off50, On250_Off70, On350_Off90, On500_Off110};
+        enum ShortBreakMode {On, Off};
 
         // constructors
-        BD65496MUV(Mode mode, uint8_t pinInA, uint8_t pinInB);                          // Constructor of the BD65496MUV class without the pwm mode pin.
-        BD65496MUV(Mode mode, uint8_t pinInA, uint8_t pinInB, uint8_t pinPwmMode);      // Constructor of the BD65496MUV class with the pwm mode pin.
-        ~BD65496MUV();                                                                  // Main destructor  of the BD65496MUV class.
+        BD65496MUV(uint8_t pinInA, uint8_t pinInB, uint8_t pinPwmMode ,DriverMode driverMode = DriverMode::Speed_n_Dir);    // Constructor of the BD65496MUV class with the pwm mode pin.
+        BD65496MUV(uint8_t pinInA, uint8_t pinInB, DriverMode driverMode = DriverMode::Speed_n_Dir,                         // Constructor of the BD65496MUV class with all pins (Pro-Mode).
+                    bool pinPwmModeDefined = false, uint8_t pinPwmMode = 0,
+                    bool pinPsDefined = false, uint8_t pinPs = 0, PowerMode powerMode = PowerMode::Running,
+                    bool pinTrDefined = false, uint8_t pinTr1 = 0, uint8_t pinTr2 = 0,
+                    SwitchingSpeed switchingSpeed = SwitchingSpeed::On150_Off50,
+                    ShortBreakMode shortBreakMode = ShortBreakMode::Off);
 
         // functions
-        uint16_t get_version();         // Get the version of the library.
+        void setSpeed(int16_t speed);                           // Set the speed of the motor.
+        int16_t getSpeed();                                     // Get the speed of the moter.
+
+        void setDriverMode(DriverMode driverMode);              // Set the driver mode of the driver.
+        DriverMode getDriverMode();                             // Get the dirver mdoe of the driver.
+
+        void setPowerMode(PowerMode powerMode);                 // Set the power mode of the driver.
+        PowerMode getPowerMode();                               // Get the power mode of the driver.
+
+        void setSwitchingSpeed(SwitchingSpeed switchingSpeed);  // Set the switching speed of the driver.
+        SwitchingSpeed getSwitchingSpeed();                     // Get the switching speed of the driver.
+
+        void setShortBreakMode(ShortBreakMode shortBreakMode);  // Set the short break mode of the driver.
+        ShortBreakMode getShortBreakMode();                     // Get the short break mode of the dirver.
+
+        uint16_t getVersion();                                  // Get the version of the library.
 
 // End PUBLIC --------------------------------------------------------------------
 
 // Begin PRIVATE -----------------------------------------------------------------        
     private:
 
+        // enum
+        enum TurnDir {CW, CCW};                 // Enum that represents the turn direction of the motor.
+
         // variables
-        Mode _driverMode;
-        uint8_t _pinInA;
-        uint8_t _pinInB;
-        uint8_t _pinPwmMode;
+        DriverMode _driverMode;                 // Var for the driver mode.
+        PowerMode _powerMode;                   // Var for the power mode.
+        SwitchingSpeed _switchingSpeed;         // Var for the switching speed.
+        ShortBreakMode _shortBreakMode;         // Var fot the short break mode.
+
+        bool _pinPwmModeDefined;                // Flag if the pwm mode pin is defined.
+        bool _pinPsDefined;                     // Flag if the ps pin is defined.
+        bool _pinTrDefined;                     // Flag if the tr pins are defined.
+
+        uint8_t _pinInA;                        // Var for the number of the INA pin.
+        uint8_t _pinInB;                        // Var for the number of the INB pin.
+        uint8_t _pinPwmMode;                    // Var for the number of the pwm mode pin.
+        uint8_t _pinPs;                         // Var for the number of the PS pin.
+        uint8_t _pinTr1;                        // Var for the number of the TR1 pin.
+        uint8_t _pinTr2;                        // Var for the number of the TR2 pin.
+
+        int16_t _speed;                         // Var for the current motor speed.
 
         // functions 
-        void _initDriverPins();
-
+        void _initDriverPins();                 // Function to initialize the output pins.
 
 // End PRIVATE -------------------------------------------------------------------
 };
